9523c6eaa686d6d6bce21aeeb7510e25da8b676e
Leonardo Issamu Monarini de Oliveira <leonardo@a9tech.com.br>
2025-03-25 20:49:42 -0300

    fix: atualiza referencia de vhost

--- access.tf
+++ access.tf
@@ -1,6 +1,6 @@
 resource "rabbitmq_permissions" "this" {
   user  = rabbitmq_user.this.name
-  vhost = var.create_vhost ? rabbitmq_vhost.this.name : var.vhost
+  vhost = var.create_vhost ? rabbitmq_vhost.this[0].name : var.vhost
 
   permissions {
     configure = ".*"
